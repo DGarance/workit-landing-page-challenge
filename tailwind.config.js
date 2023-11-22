ec345a88a256488d9585023f74fd45e74195810c
Garance Defrel <garance972@hotmail.com>
2023-11-22 21:33:30 +0100

    Mise en place challenge Frontend

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        fraunces: ["Fraunces_144, serif"],
+        manrope: ["Manrope, sans-serif"],
+      },
+      fontSize: {
+        headingL: "80px",
+        headingM: "56px",
+        headingS: "32px",
+        body: "18px",
+      },
+      lineHeight: {
+        lineL: "80px",
+        lineM: "64px",
+        lineS: "40px",
+        body: "32px",
+      },
+      colors: {
+        darkpurple: "#24053E",
+        davygrey: "#584D62",
+        eucalyptus: "#44FFA1",
+        ghostwhite:"#FCF8FF",
+        
+      },
+    },
+  },
+  plugins: [],
+};
